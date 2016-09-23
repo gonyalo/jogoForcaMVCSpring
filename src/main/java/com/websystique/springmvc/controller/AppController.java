91eb82b00ee6a7a87fc03872865142daf9a3ddc6
goncalo <joaogoncalo.borges@gmail.com>
2016-09-23 17:36:55 +0100

    letsPlay.jsp already stay in the same page and is capable of recognise the 'categoria' and the difficulty

--- src/main/java/com/websystique/springmvc/controller/AppController.java
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -197,12 +197,20 @@ public class AppController {
     @RequestMapping(value ={"/submitPair"}, method = RequestMethod.GET)
     public String submitPair(ModelMap model, @RequestParam("category") String category, @RequestParam("difficulty") String difficulty){
 
+        //This all block is from the "showLetsPlay" function //TO_DO
+        List<Teste> listaTestes = serviceTeste.findAllTestes();
+        model.addAttribute("listaTestes", listaTestes);
+        List<Category> listCategories = serviceCategory.findAllCategories();
+        model.addAttribute("listCategories", listCategories);
+        List<Difficulty> listDifficulties = serviceDifficulty.findAllDifficulties();
+        model.addAttribute("listDifficulties", listDifficulties);
+        //This all block is from the "showLetsPlay" function //TO_DO ____________END
+
 
         model.addAttribute("categoryTeste", category);
         model.addAttribute("difficultyteste", difficulty);
 
-        return ""; 
         //return "redirect:/showLetsPlay";
-        //return "letsPlay";
+        return "letsPlay";
     }
 }
