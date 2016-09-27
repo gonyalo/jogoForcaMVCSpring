f7ffb90993cf81bf65f4cab7170a767108b4f3cc
goncalo <joaogoncalo.borges@gmail.com>
2016-09-27 09:40:41 +0100

    queries.sql file

---
+++ src/queries.sql
@@ -0,0 +1,20 @@
+
+SELECT id, description
+FROM 	websystique.word as word
+	JOIN
+		(SELECT word_id as result_word_id
+		FROM websystique.word_category
+		WHERE 	difficulty_id = 	(
+						SELECT id
+						FROM websystique.difficulty
+						WHERE description like 'easy'
+						)
+			AND
+				category_id= 	(
+						SELECT id
+						FROM websystique.category
+						WHERE description like 'objecto'
+						)
+		) as result
+WHERE 	result_word_id = id
+;
